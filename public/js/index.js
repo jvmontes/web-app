195b5af59fefdce39e166926f09f27c319d11896
Jorge Viramontes <>
2022-05-20 13:49:06 -0500

    Added new coding question resolution!

--- public/js/index.js
+++ public/js/index.js
@@ -25,6 +25,14 @@ function showQuestionTwo() {
     }
 }
 
+function showQuestionThree() {
+    var questionTwoContent = document.getElementById("question-three");
+    if (questionTwoContent.style.display === "none") {
+        questionTwoContent.style.display = "block";
+    } else {
+        questionTwoContent.style.display = "none";
+    }
+}
 
 /*
  * Complete the 'countingValleys' function below.
@@ -69,3 +77,50 @@ function countingValleys() {
 
     document.getElementById("num-valleys").innerHTML = numValleys;
 }
+
+function wrap() {
+    var length = document.getElementById("length").value;
+    var width = document.getElementById("width").value;
+    var height = document.getElementById("height").value;
+
+    var totalSquareFootage = ((length * width) * 2) + ((length * height) * 2) + ((height * width) * 2);
+
+    document.getElementById("wrapping-paper").innerHTML = totalSquareFootage;
+}
+
+let arr1 = [7, 4, 10, 0, 1]
+let arr2 = [9, 7, 2, 3, 6]
+
+function maximizedArray(array1, array2) {
+    var outputArray = [];
+    var newArrayCount = 0;
+    var counterA = 0;
+    var counterB = 0;
+
+    array1 = array1.sort(function(a, b) { return b - a });
+    array2 = array2.sort(function(a, b) { return b - a });
+
+    while (newArrayCount !== array1.length) {
+        var i = array1[counterA];
+        var j = array2[counterB];
+
+        console.log(i, j);
+        if (i >= j) {
+            if (outputArray.includes(i) === false) {
+                outputArray.push(i);
+                newArrayCount++;
+            }
+            counterA++;
+        } else {
+            if (outputArray.includes(j) === false) {
+                outputArray.push(j);
+                newArrayCount++;
+            }
+            counterB++;
+        }
+    }
+
+    console.log(outputArray);
+}
+
+maximizedArray(arr1, arr2);
